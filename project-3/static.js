1db9e741cade6e87f2bad33cbcb099d5a6ee14d0
starchung7 <starchung7@gmail.com>
2022-04-25 17:43:18 -0700

    finishing up everything

--- project-3/static.js
+++ project-3/static.js
@@ -1,14 +1,15 @@
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    frameRate(5);
+    frameRate(6);
 }
 
 function draw() {
     background(0);
-    for (let a = 0; a <= 800; a = a + 30) {
+    for (let a = 0; a <= windowWidth; a = a + 30) {
         for (let b = 0; b <= 800; b = b + 10) {
             arc(a, b, random(70), random(1), random(100), random(600));
             stroke(255);
             noFill();
         }
     }
+}
